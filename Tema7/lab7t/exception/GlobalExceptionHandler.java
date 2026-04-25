dfee4efd3602a6e5adcf0425c64d7c7f1a979ca0
Ioanadora <teodoraioanapetrache@gmail.com>
2026-04-25 21:42:09 +0300

    Add files via upload

---
+++ Tema7/lab7t/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.lab7.tema7.lab7t.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleRuntime(RuntimeException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+}
