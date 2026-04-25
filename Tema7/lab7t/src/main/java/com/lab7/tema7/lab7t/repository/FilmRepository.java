75f8d8bb03e5843da6a28f51086f68dfbbd8d929
Ioanadora <teodoraioanapetrache@gmail.com>
2026-04-25 21:41:19 +0300

    Delete Tema7/lab7t directory

--- Tema7/lab7t/src/main/java/com/lab7/tema7/lab7t/repository/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lab7.tema7.lab7t.repository;
-
-import com.lab7.tema7.lab7t.model.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilmRepository extends JpaRepository<Film, Long> {
-}
